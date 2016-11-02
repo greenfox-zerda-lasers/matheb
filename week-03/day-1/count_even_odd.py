aa481f385503660e37c67a50c164e64b77ffa661
matheb <mathebogi@gmail.com>
2016-11-02 17:01:31 +0100

    2 additional tasks ready

---
+++ week-03/day-1/count_even_odd.py
@@ -0,0 +1,13 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+even = 0
+odd = 0
+
+for i in range(len(numbers)):
+  if (numbers[i])%2 == 0:
+    even = even + 1
+  else:
+    odd = odd + 1
+
+return "Number of even numbers: " + even + "."
+return "Number of odd numbers: " + odd + "."
---
+++ week-03/day-1/stars.py
@@ -0,0 +1,7 @@
+x = 4
+for i in range(10):
+    if i <=5:
+        print(i*"*")
+    else:
+        print(x*"*")
+        x = x-1
