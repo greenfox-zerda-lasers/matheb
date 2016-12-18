9f4b561d33514ddedd0496171f013c6276d23ae5
matheb <mathebogi@gmail.com>
2016-12-18 13:14:06 +0100

    Application working, better code structure needed

--- week-08/todoapp/todo.js
+++ week-08/todoapp/todo.js
@@ -1,36 +1,133 @@
-var task = {};
+var List = document.querySelector('section');
+var input = document.querySelector('input').value;
+var addButton = document.querySelector('.add');
 
 function askInputValue(){
   var newInput = document.querySelector('input').value;
-  var data = new FormData(newInput);
-  return data;
+  return newInput;
 };
+addButton.addEventListener('click', askInputValue);
 
-function askTodoListLength(){
-  var askLength = new XMLHttpRequest();
-  askLength.open('GET', "https://mysterious-dusk-8248.herokuapp.com/todos", true);
-  askLength.setRequestHeader("Content-Type: application/json");
-  askLength.send();
-  if (askLength.readyState === XMLHttpRequest.DONE){
-    var TodoList = JSON.parse(askLength.response);
-    var Listlength = TodoList.length;
-    return Listlength;
+function getList(){
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', "https://mysterious-dusk-8248.herokuapp.com/todos", true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send();
+  xhr.onreadystatechange = function (){
+    if (xhr.readyState === XMLHttpRequest.DONE){
+      var TodoList = JSON.parse(xhr.response);
+      var Listlength = TodoList.length;
+      drawList(TodoList, Listlength);
+    }
   }
 };
+getList();
+window.setInterval(getList, 5000);
 
 function addTodoItem(){
-  task['id'] = askTodoListLength()-1;
-  task['text'] = askInputValue();
-  task['completed'] = 'false';
   var req = new XMLHttpRequest();
-  request.open('POST', "https://mysterious-dusk-8248.herokuapp.com/todos", true);
-  request.setRequestHeader("Content-Type: application/json");
+  input = document.querySelector('input').value;
+  task = {text: input};
+  console.log(task);
+  req.open('POST', "https://mysterious-dusk-8248.herokuapp.com/todos", true);
+  req.setRequestHeader("Content-Type", "application/json");
+  req.onreadystatechange = function (){
+    if (req.readyState === XMLHttpRequest.DONE){
+      console.log('ok');
+      var TodoList = JSON.parse(req.response);
+      var Listlength = TodoList.length;
+      var texts = [];
+      var state = [];
+      for (var i = 0; i < Listlength; i++){
+        texts.push(TodoList[i]['text']);
+        state.push(TodoList[i]['completed']);
+      }
+      console.log(TodoList);
+      console.log(texts);
+      console.log(state);
+      getList();
+    }
+  }
   req.send(JSON.stringify(task));
-  //itt növelődik a lista div magassága + kerül bele a tartalom !!!!!
+};
+addButton.addEventListener('click', addTodoItem);
+//addTodoItem();
+
+function checkItem(item, callback){
+  var req = new XMLHttpRequest();
+  req.open('PUT', "https://mysterious-dusk-8248.herokuapp.com/todos/"+item.id, true );
+  req.setRequestHeader("content-type", "application/json; charset=utf-8");
+  req.onreadystatechange = function (){
+    if (req.readyState === XMLHttpRequest.DONE){
+      var TodoList = JSON.parse(req.response)
+      var text = TodoList['text'];
+      console.log(text)
+      console.log(TodoList);
+      console.log(JSON.stringify({id: item.id, text: item.text, completed: true}));
+      callback();
+    }
+  }
+  req.send(JSON.stringify({text: item.text, completed: !item.completed}));
 };
 
-//function delTodoItem(){};
+function removeItem(item, callback){
+  var req = new XMLHttpRequest();
+  req.open('DELETE', "https://mysterious-dusk-8248.herokuapp.com/todos/"+item.id, true);
+  req.setRequestHeader("Accept", "application/json; charset=utf-8");
+  req.onreadystatechange = function (){
+    if (req.readyState === XMLHttpRequest.DONE){
+      console.log(req.response);
+      callback();
+    }
+  }
+  req.send();
+};
 
-//function checkingTask(){};
-  //checkbox színe
-  //Completed status change
+function drawList(TodoList, Listlength){
+  List.innerHTML = '';
+  TodoList.forEach (function(item, index){
+    var newDiv = document.createElement('div');
+    newDiv.setAttribute('class', 'todo_item');
+    newDiv.setAttribute('data-index-Number', item.id);
+    newDiv.setAttribute('data-id', item.complete);
+    newDiv.dataset.indexNumber = item.id;
+
+    var newLabel = document.createElement('label');
+    newLabel.textContent =item.text.substring(0, 25);
+
+    var newIcons = document.createElement('div');
+    newIcons.setAttribute('class', 'checking');
+
+    var newButton = document.createElement('button');
+    newButton.setAttribute('class', 'del');
+    newButton.addEventListener('click', function(e){
+      removeItem(item, getList);
+    });
+
+    var newImg = document.createElement('img');
+    newImg.setAttribute('class', 'del_icon');
+    newImg.setAttribute('src', 'delete.svg');
+    newImg.setAttribute('alt', 'delete');
+    newImg.setAttribute('data-index-Number', index);
+
+
+    var newPipe = document.createElement('div');
+    newPipe.textContent = '✔';
+    if (item.completed){
+      newPipe.setAttribute('class', 'complete checked');
+    } else {
+      newPipe.setAttribute('class', 'complete');
+    }
+    newPipe.addEventListener('click', function(e){
+      console.log(index);
+      checkItem(item, getList);
+    })
+
+    newButton.appendChild(newImg);
+    newIcons.appendChild(newButton);
+    newIcons.appendChild(newPipe);
+    newDiv.appendChild(newLabel);
+    newDiv.appendChild(newIcons);
+    List.appendChild(newDiv);
+  })
+};
