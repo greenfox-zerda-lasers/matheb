aafdfae6e3dedb6bcdd718829611dc841e1d6018
matheb <mathebogi@gmail.com>
2017-02-07 15:28:27 +0100

    quick union is ready

--- week-15/quick_union.js
+++ week-15/quick_union.js
@@ -1,22 +1,36 @@
 function quickUnion(arrayLength) {
   this.array = [];
   this.length = arrayLength;
-  for ( var i = 1; i < this.length; i++) {
+  for ( var i = 1; i < this.length+1; i++) {
     this.array.push(i);
   }
+  //console.log(this.array)
 };
 //
+
+quickUnion.prototype.root = function(index){
+  //console.log(this.array[index])
+  while (index != this.array[index-1]){
+    index = this.array[index-1];
+  }
+  return index;
+};
+
+
 quickUnion.prototype.unite = function(a, b) {
-  if (a < this.length && b < this.length){
-    this.array[a] = b;
+  if (!this.find(a,b)){
+    this.array[a-1] = b;
+    return this.array;
   } else {
-    console.log('please add valid indexes');
+    console.log('Already connected');
   }
 };
 
 quickUnion.prototype.find = function(a, b) {
   if (a < this.length && b < this.length){
-    if ( this.array[a-1] === this.array[b]){
+    console.log(this.root(a));
+    console.log(this.root(b));
+    if ( this.root(a) === this.root(b)){
       return true;
     } else {
       return false;
@@ -26,31 +40,20 @@ quickUnion.prototype.find = function(a, b) {
   }
 };
 
-quickUnion.prototype.getGroups = function() {
+quickUnion.prototype.getGroups = function(){
   var groups = [];
   array = this.array;
-  array.forEach(function(groupID){
-    if(!groups.includes(groupID)){
-      groups.push(groupID);
-    }
-  })
-  return groups;
+  for ( var i = 0; i < array.length; i++){
+    groups.push(this.root(array[i]));
+  };
+  return groups.sort();
 }
 
 var myarray = new quickUnion(9);
-myarray.unite(8,1);
-console.log(myarray);
-// myarray.unite(5,2);
-// console.log(myarray);
-// myarray.unite(6,3);
-// console.log(myarray);
-// myarray.unite(8,1);
-// console.log(myarray);
-console.log("groups",myarray.getGroups());
-// 01678
-// 2
-// 3
-// 4
-// 5
+//console.log(myarray.root(3));
+//console.log(myarray.find(2,3));
+console.log(myarray.unite(8,1));
+console.log(myarray.unite(1,2));
+console.log(myarray.unite(1,8));
 
-console.log(myarray.find(2,3));
+console.log(myarray.getGroups());
